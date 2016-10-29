975f5f0e6adc43d7b0420fdb36dbbc2428384d37
Kendall Fosler <Kendall Fosler@DESKTOP-M3KM9EL>
2016-10-29 12:54:31 -0400

    Commit

--- FinalProject_TicketSystem/Program.cs
+++ FinalProject_TicketSystem/Program.cs
@@ -16,7 +16,7 @@ namespace FinalProject_TicketSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Customer());
         }
     }
 }
